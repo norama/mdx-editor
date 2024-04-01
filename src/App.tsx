155c5eccfff1dae5b5edeb0960080d4e82071bfc
Nora Mate <norama.matema@gmail.com>
2024-04-02 01:36:12 +0200

    CSS editor

--- src/App.tsx
+++ src/App.tsx
@@ -23,13 +23,15 @@ import {
 } from '@mdxeditor/editor'
 import '@mdxeditor/editor/style.css'
 import './App.css'
-import './themes/custom.css'
-import cssString from './themes/custom.css?raw'
+import defCssString from './themes/custom.css?raw'
 import PdfExportButton from './components/PdfExportButton'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+import CssEditor from 'components/CssEditor'
 
 function App() {
   const ref = useRef<MDXEditorMethods>(null)
+  const [cssString, setCssString] = useState(defCssString)
+  const [cssEditorOpen, setCssEditorOpen] = useState(false)
 
   const markdown = `
   # Items
@@ -41,55 +43,70 @@ function App() {
   
   ---
 
+  ## More Items
+
   1. Item 1
   2. Item 2
 `
 
   return (
-    <MDXEditor
-      ref={ref}
-      contentEditableClassName='awesome-editor'
-      markdown={markdown}
-      plugins={[
-        headingsPlugin(),
-        listsPlugin(),
-        quotePlugin(),
-        codeBlockPlugin(),
-        linkPlugin(),
-        linkDialogPlugin(),
-        thematicBreakPlugin(),
-        tablePlugin(),
-        imagePlugin({
-          imageUploadHandler: () => {
-            return Promise.resolve('https://picsum.photos/200/300')
-          },
-          imageAutocompleteSuggestions: [
-            'https://picsum.photos/200/300',
-            'https://picsum.photos/200',
-          ],
-        }),
-        diffSourcePlugin({ diffMarkdown: 'An older version', viewMode: 'rich-text' }),
-        toolbarPlugin({
-          toolbarContents: () => (
-            <>
-              <DiffSourceToggleWrapper>
-                <UndoRedo />
-                <BoldItalicUnderlineToggles />
-                <ListsToggle />
-                <CodeToggle />
-                <CreateLink />
-                <InsertTable />
-                <InsertImage />
-              </DiffSourceToggleWrapper>
-              <PdfExportButton
-                getMarkdown={() => ref.current?.getMarkdown() ?? ''}
-                css={cssString}
-              />
-            </>
-          ),
-        }),
-      ]}
-    />
+    <>
+      <style>{cssString}</style>
+      <MDXEditor
+        ref={ref}
+        contentEditableClassName='awesome-editor'
+        markdown={markdown}
+        plugins={[
+          headingsPlugin(),
+          listsPlugin(),
+          quotePlugin(),
+          codeBlockPlugin(),
+          linkPlugin(),
+          linkDialogPlugin(),
+          thematicBreakPlugin(),
+          tablePlugin(),
+          imagePlugin({
+            imageUploadHandler: () => {
+              return Promise.resolve('https://picsum.photos/200/300')
+            },
+            imageAutocompleteSuggestions: [
+              'https://picsum.photos/200/300',
+              'https://picsum.photos/200',
+            ],
+          }),
+          diffSourcePlugin({ diffMarkdown: 'An older version', viewMode: 'rich-text' }),
+          toolbarPlugin({
+            toolbarContents: () => (
+              <>
+                <DiffSourceToggleWrapper>
+                  <UndoRedo />
+                  <BoldItalicUnderlineToggles />
+                  <ListsToggle />
+                  <CodeToggle />
+                  <CreateLink />
+                  <InsertTable />
+                  <InsertImage />
+                </DiffSourceToggleWrapper>
+                <button onClick={() => setCssEditorOpen(true)}>CSS</button>
+                <PdfExportButton
+                  getMarkdown={() => ref.current?.getMarkdown() ?? ''}
+                  css={cssString}
+                />
+              </>
+            ),
+          }),
+        ]}
+      />
+      <CssEditor
+        key={cssString}
+        open={cssEditorOpen}
+        css={cssString}
+        onCssChange={(newCssString) => {
+          setCssString(newCssString)
+          setCssEditorOpen(false)
+        }}
+      />
+    </>
   )
 }
 
---
+++ src/components/CssEditor.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Drawer from 'react-modern-drawer'
+import 'react-modern-drawer/dist/index.css'
+// @ts-expect-error no types for react-style-editor
+import StyleEditor from 'react-style-editor'
+
+interface ICssEditor {
+  open: boolean
+  css: string
+  onCssChange: (newCss: string) => void
+}
+
+const CssEditor = ({ open, css, onCssChange }: ICssEditor) => {
+  const [currentCss, setCurrentCss] = useState(css)
+
+  return (
+    <Drawer open={open} onClose={() => onCssChange(currentCss)} direction='bottom'>
+      <StyleEditor value={currentCss} onChange={(value: string) => setCurrentCss(value)} />
+    </Drawer>
+  )
+}
+
+export default CssEditor
