766d8f81d98c52c78de324119bd64d6c18828291
Nora Mate <norama.matema@gmail.com>
2024-04-02 23:11:16 +0200

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  base: '/mdx-editor/',
   server: {
     port: 3000,
   },
