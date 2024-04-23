0b2966e6a000a080fcaa256ca89f2a69f86e6272
Nora Mate <norama.matema@gmail.com>
2024-04-23 23:27:44 +0200

    pdfExport img width height in CSS

--- src/api/pdfExport.ts
+++ src/api/pdfExport.ts
@@ -2,9 +2,41 @@
 
 const PDF_EXPORT_URL = 'https://md-to-pdf.fly.dev'
 
-const pdfExport = async (markdown: string, css?: string) => {
+// https://www.regextester.com/103676
+const IMG_REGEX = /<img.*?src="(.*?)"[^>]+>/g
+const WIDTH_REGEX = /width="([0-9]*)"/i
+const HEIGHT_REGEX = /height="([0-9]*)"/i
+
+type TMatchResult = (RegExpMatchArray | null)[] | undefined
+
+const toCss = (attrs: TMatchResult, name: string) =>
+  attrs
+    ?.filter((attr) => attr !== null)
+    .map(
+      (attr) => `
+        img[${attr[0]}] {
+          ${name}: ${attr[1]}px;
+        }`,
+    )
+    .join('\n')
+
+const toWidthCss = (attrs: TMatchResult) => toCss(attrs, 'width')
+const toHeightCss = (attrs: TMatchResult) => toCss(attrs, 'height')
+
+const imgCss = (markdown: string) => {
+  const imgTags = markdown.match(IMG_REGEX)
+  const widthAttrs = imgTags?.map((img) => img.match(WIDTH_REGEX))
+  const heightAttrs = imgTags?.map((img) => img.match(HEIGHT_REGEX))
+  const widthCss = toWidthCss(widthAttrs) ?? ''
+  const heightCss = toHeightCss(heightAttrs) ?? ''
+  return [widthCss, heightCss].join('\n').trim()
+}
+
+const pdfExport = async (markdown: string, css = '') => {
   console.log('markdown', markdown)
   console.log('css', css)
+
+  css += imgCss(markdown)
 
   const data = new URLSearchParams()
   data.append('markdown', markdown)
